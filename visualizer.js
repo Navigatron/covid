f772e16ac4b3125b037e939527dc725fcf3090bc
Ethan Witherington <wyddindustries@gmail.com>
2020-04-15 12:05:11 -0400

    april 14th data has been published

--- visualizer.js
+++ visualizer.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // When the page loads, what data do we show first?
-let defaultDisplayDate = "2020-04-13";
+let defaultDisplayDate = "2020-04-14";
 
 // default map size
 let width = 960;
